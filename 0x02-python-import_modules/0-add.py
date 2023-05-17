f72867f70df47b62f11f1997334d6bdb891af905
reckafella <ethanmuthoniw6@gmail.com>
2023-05-17 14:14:17 +0300

    update 0-add.py to print integers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a, b = 1, 2
+a = 1
+b = 2
 
-if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

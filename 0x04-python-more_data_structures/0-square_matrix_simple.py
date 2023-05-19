ea11af82d4ce06629213329c103759578e1530e9
ethan muthoni <ethan.muthoni@pm.me>
2023-05-19 21:13:29 +0300

    first commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def square_matrix_simple(matrix=[]):
+    matrix_copy = []
+
+    
+
+    return (matrix_copy)
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(square_matrix_simple(matrix))
+print(matrix)

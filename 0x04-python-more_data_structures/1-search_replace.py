71693fbe4508cc0b87009ae95008bab9b6a8ffc4
reckafella <ethanmuthoniw6@gmail.com>
2023-05-19 22:23:26 +0300

    add 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+
+    for i in range(len(my_list)):
+        new_list[i] = my_list[i]
+        if (my_list[i] == search):
+            new_list[i] = replace
+
+    return new_list

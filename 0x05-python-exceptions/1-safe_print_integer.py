4c7462001afc442af189dcd61bc75cd4d3aee199
ethan muthoni <ethan.muthoni@pm.me>
2023-05-30 01:43:26 +0300

    update 1-safe_print_*.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,8 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        return True
     except ValueError:
         return False
+    else:
+        return True
+

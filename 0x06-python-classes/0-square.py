61231e21f5a51ed1c6479871d513200e0ea25ba4
reckafella <ethanmuthoniw6@gmail.com>
2023-05-30 22:53:57 +0300

    update 0-square.py by adding module docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
+"""
+This module define a class Square which is used to handle the
+the calculation of area of a square.
+"""
 class Square:
     """
     An empty class that defines a square. The class does not do anything.
     The class has no attributes or properties.
     There is no need to use Attributes and @property identifies.
     """
-
     pass

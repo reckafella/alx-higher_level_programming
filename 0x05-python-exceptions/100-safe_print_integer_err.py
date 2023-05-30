ebb8bddf6407befa7a38a422717b38a01ac0ef18
ethan muthoni <ethan.muthoni@pm.me>
2023-05-30 04:12:07 +0300

    final edit 100-safe*.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
-import sys, traceback
+import sys
 
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
-        print("Exception: {}".format(traceback.
-                                     format_exc().
-                                     strip().
-                                     splitlines()[-1][12:]), file=sys.stderr)
+    except ValueError as ve:
+        print("Exception: {}".format(ve), file=sys.stderr)
         return False

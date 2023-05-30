e5addfc9bd7b4288fd0b73780e3ba7b19a3b23b9
reckafella <ethanmuthoniw6@gmail.com>
2023-05-30 23:11:13 +0300

    update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,15 +9,21 @@ the calculation of area of a square.
 class Square:
     """
     A class that defines a square.
+
+    Args:
+        size (int): size of a single side of a square
+
+    Attributes:
+        size (int): size of a single side of a square
     """
     def __init__(self, size):
         """
         The __init__ method
-        
+
         Args:
-        size (int): size of a single side of a square
-        
+            size (int): size of a single side of a square
+
         Attributes:
-        size (int): size of a single side of a square
+            size (int): size of a single side of a square
         """
         self._size = size

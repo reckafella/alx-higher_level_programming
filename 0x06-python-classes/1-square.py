35a3455932cae358c638e0612b128138a7211ff7
reckafella <ethanmuthoniw6@gmail.com>
2023-05-30 23:08:42 +0300

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module define a class Square which is used to handle the
+the calculation of area of a square.
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+    """
+    def __init__(self, size):
+        """
+        The __init__ method
+        
+        Args:
+        size (int): size of a single side of a square
+        
+        Attributes:
+        size (int): size of a single side of a square
+        """
+        self._size = size

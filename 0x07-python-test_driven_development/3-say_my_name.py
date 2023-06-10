f2e55a884881a718a71a26c53b987933f9c020fe
reckafella <ethanmuthoniw6@gmail.com>
2023-06-10 04:40:16 +0300

    update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,8 +13,9 @@ def say_my_name(first_name, last_name=""):
         first_name: string
         last_name: string
     """
-    if first_name == None:
-        raise TypeError("say_my_name() missing 1 required positional argument: 'first_name'")
+    if first_name is None:
+        raise TypeError("say_my_name() missing 1 required positional\
+                        argument: 'first_name'")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

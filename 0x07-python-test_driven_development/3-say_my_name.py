8ba8f363177c4bc37a078bfc19e98a0f2bddb2e2
reckafella <ethanmuthoniw6@gmail.com>
2023-06-10 04:28:47 +0300

    add 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+The module contains a function that prints My name is <first name> <last name>
+Prototype: def say_my_name(first_name, last_name=""):
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print the string: My name is <first_name> <last_name>
+    Args:
+        first_name: string
+        last_name: string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))

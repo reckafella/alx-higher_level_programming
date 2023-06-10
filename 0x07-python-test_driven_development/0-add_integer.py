b4a4ffe04fe8410818717af0aa2cb2f944e4cb60
reckafella <ethanmuthoniw6@gmail.com>
2023-06-10 03:07:34 +0300

    make more changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,15 +8,12 @@ Function: add_integer
 
 
 def add_integer(a, b=98):
-    """
-    Return the sum of integer values a and b
+    """Return the sum of integer values a and b
     Args:
-        a (int): integer
-        b (int): integer
-    """
-    if not isinstance(a, (int, float)) or type(a) == type(True):
+        a (int): integer, b (int): integer"""
+    if not isinstance(a, (int, float)) or isinstance(a, bool):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or type(b) == type(True):
+    if not isinstance(b, (int, float)) or isinstance(b, bool):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)

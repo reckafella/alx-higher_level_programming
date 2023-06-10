ce6d6711e5f71e9d2703a04d2528f19efeb12a43
reckafella <ethanmuthoniw6@gmail.com>
2023-06-10 05:58:15 +0300

    push 5-tex*.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Module contains a function that prints a text with 2 new lines after each of\
+these characters: ., ? and :
+
+Prototype: def text_indentation(text):
+"""
+
+
+def text_indentation(text):
+    """
+    Print a text with 2 new lines after each of
+    these characters: ., ? and :
+    Args:
+        text: string
+    """
+    
+    if text is None:
+        raise TypeError("text_indentation() missing 1 required positional argument: 'text'")
+    elif not isinstance(text, str):
+        raise TypeError("text must be a string")
+    else:
+        for char in text:
+            if (char == '.' or char == '?' or char == ':'):
+                print()
+                print()
+            else:
+                print("{}".format(char), end="")

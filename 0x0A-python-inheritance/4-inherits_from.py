3a41435453d67cc083c02ec55bd8c86a1e406967
reckafella <ethanmuthoniw6@gmail.com>
2023-06-13 01:53:25 +0300

    add 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Module contains function that checks if an object's class is a subclass of\
+a specified class """
+
+
+def inherits_from(obj, a_class):
+    """
+    Return True if obj is an instance of a_class. False, otherwise.
+
+    Args:
+        obj: an object
+        a_class: Python class
+    """
+    return (issubclass(type(obj), a_class) and not type(obj) == a_class)

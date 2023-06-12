d1e827336d20ca0f92383a4c308a469c960c2ab3
reckafella <ethanmuthoniw6@gmail.com>
2023-06-13 01:30:40 +0300

    add 3-is_kind_of_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-""" Module contains function that checks if an object is an instance of a
+""" Module contains function that checks if an object is a type of a
 specified class """
 
 
 def is_same_class(obj, a_class):
     """
-    Return True if obj is an instance of a_class. False, otherwise.
+    Return True if obj is a type of a_class. False, otherwise.
 
     Args:
         obj: an object
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Module contains function that checks if an object is an instance of a
+specified class """
+
+
+def is_same_class(obj, a_class):
+    """
+    Return True if obj is an instance of a_class. False, otherwise.
+
+    Args:
+        obj: an object
+        a_class: Python class
+    """
+    return isinstance(obj, a_class)

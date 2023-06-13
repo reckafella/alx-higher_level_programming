aa6afdd9eb09d11f5ca8cff35f79db8a5b5e8c02
reckafella <ethanmuthoniw6@gmail.com>
2023-06-13 04:13:43 +0300

    check check

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,7 +10,6 @@ class Rectangle(BaseGeometry):
     """ This class extends BaseGeometry """
 
     def __init__(self, width, height):
-        super().__init__()
         self.__width = super().integer_validator("width", width)
         self.__height = super().integer_validator("height", height)
 

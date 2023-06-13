f792ea5b30065d55229bafd2050d9690e261f81d
reckafella <ethanmuthoniw6@gmail.com>
2023-06-13 03:45:17 +0300

    add tests for 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -13,7 +13,9 @@ class MyList(list):
     """
 
     def __init__(self):
+        """ Initialize MyList by inheriting the properties of list """
         super().__init__(self)
 
     def print_sorted(self):
+        """ Print the list in ascending order """
         print(sorted(self))

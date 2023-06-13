e85a0d1a1376e77fab410c9950181e9d6001035f
reckafella <ethanmuthoniw6@gmail.com>
2023-06-13 05:33:46 +0300

    add 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" This module creates a class Rectangle that inherits properties of\
+BaseGeometry """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ This class extends Rectangle """
+
+    def __init__(self, size):
+        """ Initialize the class """
+        self.__size = super().integer_validator("size", size)
+
+    def area(self):
+        """ Return the area of the rectangle """
+        if isinstance(self.__size, int):
+            return (self.__size * self.__size)
+
+    def __str__(self):
+        """ Return Rectangle width and height """
+        return ("[{}] {}/{}".format(self.__class__.__bases__[0].__name__,
+                                    self.__size, self.__size))

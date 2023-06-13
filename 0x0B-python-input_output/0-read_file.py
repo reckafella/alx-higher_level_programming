4c2eaa17f3e426b9fc62e01246fd31e9c3478f6c
ethan muthoni <ethan.muthoni@pm.me>
2023-06-13 23:50:03 +0300

    add 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" Module contains a function to read and print contents of a text file """
+
+
+def read_file(filename=""):
+    """
+    Read and print contents of a text file
+
+    Args:
+        filename (str): name of file to open
+    """
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        contents = f.read()
+        print("{}".format(contents))

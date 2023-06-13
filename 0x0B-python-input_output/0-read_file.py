2f192dd978cb4b4c562d4527f4da493fdc1e4ff7
ethan muthoni <ethan.muthoni@pm.me>
2023-06-14 00:31:55 +0300

    last update on 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,6 +12,4 @@ def read_file(filename=""):
     """
 
     with open(filename, encoding='UTF8') as f:
-        for line in f:
-            print("{}".format(line), end='')
-        print()
+        print(f.read())

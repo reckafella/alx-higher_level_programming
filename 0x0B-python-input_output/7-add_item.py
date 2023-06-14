e3e475894484bc8cf94fda27c7ae1c1ff2962710
ethan muthoni <ethan.muthoni@pm.me>
2023-06-15 01:15:15 +0300

    update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,17 +9,12 @@ file_loader = __import__("6-load_from_json_file").load_from_json_file
 file_saver = __import__("5-save_to_json_file").save_to_json_file
 
 filename = "add_item.json"
-if __name__ == "__main__":
-    num_args = len(sys.argv)
 
-    arg_list = []
-
-    try:
-        json_list = list(file_loader(filename))
-    except FileNotFoundError:
-        file_saver(arg_list, filename)
-    else:
-        for arg in range(1, num_args):
-            arg_list.append(sys.argv[arg])
-        json_list = (json_list) + arg_list
-        file_saver(json_list, filename)
+try:
+    json_list = file_loader(filename)
+except FileNotFoundError:
+    json_list = []
+    file_saver(json_list, filename)
+else:
+    json_list.extend(sys.argv[1:])
+    file_saver(json_list, filename)

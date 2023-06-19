d545f3e375735b94b7797e485769f1c8b2c4010a
reckafella <ethanmuthoniw6@gmail.com>
2023-06-20 02:30:29 +0300

    add base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Module contains the Base class that is imported by other classes """
+
+class Base:
+    """Base class
+
+    Attributes:
+        id (int): integer value
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ class constructor used to initialize each object """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

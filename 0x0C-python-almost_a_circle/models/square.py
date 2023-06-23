d27cf88d7b10c179762888e973fef15c98ecc78c
reckafella <ethanmuthoniw6@gmail.com>
2023-06-24 01:25:55 +0300

    add setters and getters to Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initialize Square by using logic of __init__ from Rectangle class
+
+        Args:
+            size (int)
+            x (int)
+            y (int)
+            id (int)
         """
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
@@ -17,3 +23,18 @@ class Square(Rectangle):
         return '[{}] ({}) {}/{} - {}'.format(self.__class__.__name__,
                                              self.id, super().x,
                                              super().y, super().height)
+
+    @property
+    def size(self):
+        """ Return the value of size """
+        return super().width
+    
+    @size.setter
+    def size(self, size):
+        """
+        Sets the value of width and height to size
+
+        Args:
+            size (int): length of one side of square
+        """
+        Rectangle(width=size, height=size)

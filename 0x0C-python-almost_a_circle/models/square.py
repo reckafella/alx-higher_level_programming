0003e86bd5e7f2cccf2c7dc33e5df37b2bfccf09
reckafella <ethanmuthoniw6@gmail.com>
2023-06-24 01:05:37 +0300

    add __str__ method to Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 """ Module contains Square class that extends Rectangle """
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """ Class extends Rectangle """
     def __init__(self, size, x=0, y=0, id=None):
@@ -10,3 +11,9 @@ class Square(Rectangle):
         Initialize Square by using logic of __init__ from Rectangle class
         """
         super().__init__(width=size, height=size, x=x, y=y, id=id)
+    
+    def __str__(self):
+        """ Override __str__ method from Rectangle class """
+        return '[{}] ({}) {}/{} - {}'.format(self.__class__.__name__,
+                                             self.id, super().x,
+                                             super().y, super().height)

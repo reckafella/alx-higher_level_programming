a0120a7fd38c81fa99d06f19daf8b3a33370545c
reckafella <ethanmuthoniw6@gmail.com>
2023-06-24 02:31:56 +0300

    add update method in Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,38 @@ class Square(Rectangle):
         Rectangle(width=size, height=size)
         Rectangle.width = size
         Rectangle.height = size
+
+    def update(self, *args, **kwargs):
+        """
+        assigns attributes:
+
+        *args is the list of arguments - no-keyworded arguments
+            1st argument should be the id attribute
+            2nd argument should be the size attribute
+            3rd argument should be the x attribute
+            4th argument should be the y attribute
+        **kwargs must be skipped if *args exists and is not empty
+        """
+        if (args is not None and len(args) > 0):
+            for i in range(len(args)):
+                if i == 0:
+                    Rectangle.update(self, id=args[i])
+                if i == 1:
+                    Rectangle.update(self, width=args[i], height=args[i])
+                if i == 2:
+                    Rectangle.update(self, x=args[i])
+                if i == 3:
+                    Rectangle.update(self, y=args[i])
+        else:
+            if (kwargs is not None and len(kwargs) > 0):
+                for key, value in kwargs.items():
+                    if key == 'id':
+                        Rectangle.update(self, id=value)
+                    if key == 'size':
+                        Rectangle.update(self, width=value, height=value)
+                    if key == 'x':
+                        Rectangle.update(self, x=value)
+                    if key == 'y':
+                        Rectangle.update(self, y=value)
+
+

66a070cf9a9621bd31cc793547e3046a38fbfa6b
reckafella <ethanmuthoniw6@gmail.com>
2023-07-19 00:57:23 +0300

    update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -2,7 +2,4 @@
 -- If the database hbtn_0c_0 already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 
-IF DB_ID('hbtn_0c_0') IS NULL
-BEGIN
-	CREATE DATABASE hbtn_0c_0;
-END
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;

16f2929737194c9020f2976b9ca5ca8aeae82b31
reckafella <ethanmuthoniw6@gmail.com>
2023-07-20 22:51:51 +0300

    update 9-*.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -16,7 +16,4 @@
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 
 INSERT INTO TABLE `second_table` (`id`, `name`, `score`)
-VALUES (1, "John", 10),
-    (2, "Alex", 3),
-    (3, "Bob", 14),
-    (4, "George", 8);
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);

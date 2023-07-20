0e86f8580c249abc1bbc748a0700eac51cb34d93
reckafella <ethanmuthoniw6@gmail.com>
2023-07-21 00:31:58 +0300

    add 15-groups.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,12 @@
+-- a script that lists the number of records with the same score in the table `second_table` of the database
+-- `hbtn_0c_0` in the MYSQL server.
+
+-- The result should display:
+    -- the `score`
+    -- the number of records for this `score` with the label `number`
+-- the list should be sorted by the number of records (desc)
+-- database name passed as an argument of the `mysql` command.
+
+SELECT DISTINCT `score`, COUNT(`score`) AS `number`
+FROM `second_table`
+ORDER BY `number` DESC;

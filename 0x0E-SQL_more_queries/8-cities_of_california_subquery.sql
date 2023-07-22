a9e314378e122a1b0e5204debabbfbb38c9f40ed
reckafella <ethanmuthoniw6@gmail.com>
2023-07-22 21:32:07 +0300

    UPDATE 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,7 +5,9 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-SELECT `cities`.`id`, `cities`.`name`
+SELECT `id`, `name`
 FROM `cities`
-INNER JOIN `states`
-ON `states`.`id` = `cities`.`state_id`;
+WHERE `state_id` = (
+    SELECT `id` FROM `states` WHERE `name` = 'California';
+)
+ORDER BY `cities`.`id` ASC;

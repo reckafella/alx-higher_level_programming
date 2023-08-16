30f76cb39a66d3acc6d60d798acb1174049f249a
reckafella <ethanmuthoniw6@gmail.com>
2023-08-17 02:08:24 +0300

    update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,7 +10,6 @@ module.exports = class Rectangle {
   }
 
   print () {
-    // prints the rectangle using the character X
     for (let i = 0; i < this.height; i++) {
       const array = [];
       for (let j = 0; j < this.width; j++) {
@@ -21,14 +20,12 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    // exchanges the width and the height of the rectangle
     this.#temp = this.height;
     this.height = this.width;
     this.width = this.#temp;
   }
 
   double () {
-    // multiples the width and the height of the rectangle by 2
     this.height *= 2;
     this.width *= 2;
   }

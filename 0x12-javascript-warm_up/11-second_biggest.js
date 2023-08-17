4cad4a432b86e2d3feee9567085d588cb15b95fd
reckafella <ethanmuthoniw6@gmail.com>
2023-08-17 04:34:30 +0300

    update 11*.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 const argsNum = args.length;
 
-if (argsNum <= 1) {
+if (argsNum === 1 || argsNum === 0) {
   console.log(0);
 } else {
   console.log(args.sort()[argsNum - 2]);

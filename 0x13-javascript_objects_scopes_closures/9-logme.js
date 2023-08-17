9f391e15621e7b96fb846bbea4a18e1785742465
reckafella <ethanmuthoniw6@gmail.com>
2023-08-17 04:19:03 +0300

    touche; bad bad.

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+  const list = [];
+  list.push(item);
+  let listLen = list.length;
+  let itr = 0;
+
+  while (itr < listLen) {
+    console.log(itr + ': ' + list[itr]);
+    ++itr;
+  }
+};

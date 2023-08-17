0012009fac454e8831629670cb755d41927aff7e
reckafella <ethanmuthoniw6@gmail.com>
2023-08-17 03:27:12 +0300

    add 7-occurences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  const listLen = list.length;
+  let nOccurs = 0;
+
+  for (let i = 0; i < listLen; i++) {
+    if (list[i] === searchElement) {
+      nOccurs += 1;
+    }
+  }
+  return (nOccurs);
+}

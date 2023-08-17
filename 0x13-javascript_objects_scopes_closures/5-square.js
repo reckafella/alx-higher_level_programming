02a7045094d162ae23ec1dd7a788ae91d74ee35a
reckafella <ethanmuthoniw6@gmail.com>
2023-08-17 03:05:45 +0300

    more updates to the files

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,5 +3,8 @@
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) { super(size, size); }
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,22 +3,22 @@
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) { 
+  constructor (size) {
     super(size, size);
     this.size = size;
   }
 
-  charPrint(c) {
+  charPrint (c) {
     if (!c) {
-      super.print()
+      super.print();
     } else {
       for (let i = 0; i < this.size; i++) {
-      const array = [];
-      for (let j = 0; j < this.size; j++) {
-        array.unshift(c);
+        const array = [];
+        for (let j = 0; j < this.size; j++) {
+          array.unshift(c);
+        }
+        console.log(array.join(''));
       }
-      console.log(array.join(''));
-    }
     }
   }
 };

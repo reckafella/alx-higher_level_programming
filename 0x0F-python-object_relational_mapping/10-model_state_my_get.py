68a277dbc9e8d03b70b004ac3405547ad153c9e9
reckafella <ethanmuthoniw6@gmail.com>
2023-08-22 19:54:07 +0300

    add 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+Script that lists all `State` objects that contain the letter `a`\
+        from the database `hbtn_0e_6_usa`
+'''
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv as av
+from sqlalchemy.sql import text
+from sqlalchemy.orm.exc import NoResultFound
+
+
+if __name__ == '__main__':
+    # Create a database engine
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(av[1], av[2], av[3])
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    # Create a base class for declarative models
+    Base.metadata.create_all(engine)
+
+    # create session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # pull all records from the database
+    try:
+        result = session.query(State).filter(text("name=:name"))\
+        .params(name=av[4]).one()
+
+        print('{}'.format(result.id))
+    except NoResultFound:
+        print('Not Found')
+
+    session.close()

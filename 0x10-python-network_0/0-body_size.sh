d2a51aa0f3e1ee3297e3b9ddc776728f7451a733
reckafella <ethanmuthoniw6@gmail.com>
2023-09-01 01:43:08 +0300

    add 0-body_size.sh

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+curl "$1" -sI | grep -iF "Content-Length" | grep -oP "\d+"

5ee1104a228fa6d646209cdc6de151f1ad5c14b0
reckafella <ethanmuthoniw6@gmail.com>
2023-09-01 04:16:14 +0300

    update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -s --url 0.0.0.0:5000/catch_me -d "You got me!"
+curl -s --url 0.0.0.0:5000/catch_me --request "You got me!"

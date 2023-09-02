061838aaa2ee8b26c709781e4938c843d367dcd3
reckafella <ethanmuthoniw6@gmail.com>
2023-09-02 03:34:26 +0300

    update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,5 +11,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     with requests.get(url) as response:
-        x_request_id = response.headers['X-Request-Id']
+        x_request_id = response.headers.get('X-Request-Id')
         print('{}'.format(x_request_id))

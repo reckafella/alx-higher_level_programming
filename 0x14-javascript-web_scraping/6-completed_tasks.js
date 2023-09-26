2c32776f1f30de293607a1fa882f30bafa04e90d
reckafella <ethanmuthoniw6@gmail.com>
2023-09-27 02:21:36 +0300

    add 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+
+const args = process.argv.slice(2);
+const requestURL = args[0];
+
+request(requestURL, function (_err, _response, body) {
+  const users = JSON.parse(body);
+
+  const completedJobs = {};
+
+  for (const user of users) {
+    const userID = user.userId;
+    const completed = user.completed;
+
+    if (!completedJobs[userID]) {
+      completedJobs[userID] = 0;
+    }
+
+    if (completed) {
+      completedJobs[userID]++;
+    }
+  }
+
+  for (const userID in completedJobs) {
+    console.log(`'${userID}', ${completedJobs[userID]}`);
+  }
+});
